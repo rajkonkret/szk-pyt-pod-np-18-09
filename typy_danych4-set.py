8ac0bd171466606b4609413020c94d1c1eba63f5
CSComarch <rajkonkret660@gmail.com>
2023-09-19 10:05:21 +0200

    2 - add set

--- typy_danych4-set.py
+++ typy_danych4-set.py
@@ -14,3 +14,29 @@ zbior.add(18)
 zbior.add(18)
 print(zbior)  # {33, 66, 777, 11, 44, 18, 22, 55}
 # test
+
+zbior.remove(55)  # usuniécie bo elemencie
+print(zbior)  # {33, 66, 777, 11, 44, 18, 22}
+
+print(zbior.pop())  # usuniecie pierwszego elemntu ze zbioru, 33
+print(zbior)  # {66, 777, 11, 44, 18, 22}
+zbior.pop()
+print(zbior)  # {777, 11, 44, 18, 22}
+
+print(type(zbior))  # <class 'set'>
+
+lista2 = list(zbior)
+print(lista2)  # [777, 11, 44, 18, 22]
+
+zbior2 = {66, 11, 44, 18, 52, 62, 999, 999}
+print(zbior2)  # {66, 18, 52, 999, 11, 44, 62}
+
+print(zbior | zbior2)  # suma zbiorów, {66, 999, 777, 11, 44, 18, 52, 22, 62}
+print(zbior & zbior2)  # część wspólna {18, 11, 44}
+print(zbior)  # {777, 11, 44, 18, 22}
+print(zbior2)  # {66, 18, 52, 999, 11, 44, 62}
+print(zbior - zbior2)  # różnica {777, 22}
+# {777, 22}
+print(zbior.difference(zbior2))  # {777, 22}
+print(zbior2.difference(zbior))  # {66, 52, 62, 999}
+# {66, 18, 52, 999, 11, 44, 62} - {777, 11, 44, 18, 22} = {66, 52, 999, 62}
