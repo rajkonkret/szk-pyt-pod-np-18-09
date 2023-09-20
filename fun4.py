f6afcd1aefbfae87bc51bb11388be9073e8ea153
CSComarch <rajkonkret660@gmail.com>
2023-09-20 10:43:33 +0200

    3 - add fun4

---
+++ fun4.py
@@ -0,0 +1,14 @@
+# funkcje zagnieżdzone
+def fun1():
+    print("To jest fun1")
+
+    def fun2():
+        print("To jest fun2")
+
+    return fun2  # bez () - ma zwrócic tylko adres gdzie sie funkcja znajduje. nie uruchamiac.
+
+
+xFun = fun1()
+print(xFun)  # <function fun1.<locals>.fun2 at 0x00000139E1678860>
+print(type(xFun))  # <class 'function'>
+xFun()  # ()  - powodują uruchomienie funkcji zawartej w xFun czyli fun2
