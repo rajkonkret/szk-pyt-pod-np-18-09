5b8460501fbaf95472028c04a984655db61ac595
CSComarch <rajkonkret660@gmail.com>
2023-09-20 11:19:18 +0200

    3 - add fun4

--- fun4.py
+++ fun4.py
@@ -12,3 +12,4 @@ xFun = fun1()
 print(xFun)  # <function fun1.<locals>.fun2 at 0x00000139E1678860>
 print(type(xFun))  # <class 'function'>
 xFun()  # ()  - powodują uruchomienie funkcji zawartej w xFun czyli fun2
+# 11:15
