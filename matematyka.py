cc896515dadc89071d93de0b191d471296977f05
CSComarch <rajkonkret660@gmail.com>
2023-09-20 15:08:27 +0200

    3 -add matematyka

---
+++ matematyka.py
@@ -0,0 +1,11 @@
+import math
+
+pi = math.pi
+print(pi)  # 3.141592653589793
+
+print(math.sqrt(1025))  # 32.01562118716424
+print(math.cos(50))  # 0.9649660284921133
+print(math.pow(1.7 * 10 ** 308, 1))  # 1.7e+308^1
+
+print(math.pow(1.8 * 10 ** 308, 1))  # inf
+print(math.pow(2, 10))  # 1024.0
