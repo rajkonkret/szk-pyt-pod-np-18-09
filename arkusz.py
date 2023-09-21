6dc82a8d52c654c676f508c3e71e66e064d4251d
CSComarch <rajkonkret660@gmail.com>
2023-09-21 15:15:59 +0200

    4 - add arkusz.py

---
+++ arkusz.py
@@ -0,0 +1,42 @@
+import openpyxl
+
+# pip install openpyxl
+
+workbook = openpyxl.load_workbook('sales.xlsx')
+worksheet = workbook.active
+
+print(worksheet)  # <Worksheet "Arkusz1">
+
+lista = []
+for w in worksheet:
+    print(w)  # (<Cell 'Arkusz1'.A1>, <Cell 'Arkusz1'.B1>, <Cell 'Arkusz1'.C1>)
+    print(type(w))  # <class 'tuple'>
+
+for i in range(0, worksheet.max_row):
+    for col in worksheet.iter_cols(1, worksheet.max_column):
+        lista.append(col[i].value)
+
+print(lista)
+print(lista[0])
+print(lista[0:3])  # ['Sales Date', 'Sales Person', 'Amount']
+
+for i in range(0, len(lista), 3):  # start, stop, step(krok)
+    print(lista[i:i + 3])
+
+# ['Sales Date', 'Sales Person', 'Amount']
+# [datetime.datetime(2018, 5, 12, 0, 0), 'Sila Ahmed', 60000]
+# [datetime.datetime(2019, 12, 6, 0, 0), 'Mir Hossain', 50000]
+# [datetime.datetime(2020, 8, 9, 0, 0), 'Sarmin Jahan', 45000]
+# [datetime.datetime(2021, 4, 7, 0, 0), 'Mahmudul Hasan', 30000]
+
+value = worksheet['A1'].value
+
+print(value)  # Sales Date
+value = worksheet[1]
+print(value) # (<Cell 'Arkusz1'.A1>, <Cell 'Arkusz1'.B1>, <Cell 'Arkusz1'.C1>)
+for cell in worksheet[1]:
+    print(cell.value)
+
+# Sales Date
+# Sales Person
+# Amount
