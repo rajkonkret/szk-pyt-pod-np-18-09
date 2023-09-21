a3e00af292b476d3a6fc3275e9c83dcda4659247
CSComarch <rajkonkret660@gmail.com>
2023-09-21 12:53:40 +0200

    4 - add kl9

---
+++ kl9-exception.py
@@ -0,0 +1,15 @@
+class MyException(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+
+try:
+    # print(2 / 0)  # Bład division by zero
+    # raise ZeroDivisionError  # Bład
+    # raise ZeroDivisionError("Nie dziel przez zero") Bład Nie dziel przez zero
+
+    raise MyException("Wystąpił wyjątek")  # MyException: Wystąpił wyjątek
+except MyException:
+    print("Wystapil wyjątek MyException")  # Wystapil wyjątek MyException
+except Exception as e:
+    print("Bład", e)
