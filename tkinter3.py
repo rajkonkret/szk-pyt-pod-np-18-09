d0c15ac4237006565279b67af8b223041b57ba37
CSComarch <rajkonkret660@gmail.com>
2023-09-22 15:25:23 +0200

    5 -add tkinter3

---
+++ tkinter3.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+
+def show_text():
+    text = entry.get()
+    print(f"Wprowadzony tekst: {text}")
+
+
+app= tk.Tk()
+app.title("Pole wprowadzania")
+
+entry = tk.Entry(app)
+entry.pack()
+
+button = tk.Button(app, text="Pokaż tekst", command=show_text)
+button.pack(side=tk.BOTTOM)  # "bottom" -> tk.BOTTOM, enumy
+
+app.mainloop()
