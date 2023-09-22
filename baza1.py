74d82963e33b7ac2a8276fc9bf429d091b45b651
CSComarch <rajkonkret660@gmail.com>
2023-09-22 10:28:27 +0200

    5 - add baza1

---
+++ baza1.py
@@ -0,0 +1,14 @@
+# relacyjne bazy danych sql
+
+import sqlite3
+
+try:
+    sql_connection = sqlite3.connect('sqlite_python.db')
+    cursor = sql_connection.cursor()
+    print("Baza danych została podłaczona")
+except sqlite3.Error as e:
+    print("Bład podczas podłaczania bay danych", e)
+finally:
+    if sql_connection:
+        sql_connection.close()
+        print("Baza danych został zamknięta")
